32dda57123dad607a79e5714aa5abc6d910db096
neha-081 <nehaprajapati1150@gmail.com>
2022-02-28 16:31:04 +0530

    add payment page

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -7,14 +7,11 @@ import ErrorPage from '../Pages/ErrorPage';
 import ProtectedRoutes from './ProtectedRoutes';
 import HomeProdDetail from '../Components/HomePage/homeProdDetail';
 import { Cartbtn } from '../Pages/cartbtn';
-// import PaymentPage from '../Pages/Payment';
-
+import { Payment } from '../Components/Payment';
 
 // todo : Merge
 import Login from '../Pages/Login';
 import Register from '../Pages/Register';
-
-
 
 const RoutesContainer = () => {
   const isLoggedIn = false;
@@ -22,15 +19,16 @@ const RoutesContainer = () => {
   return (
     <div>      
         <Routes>
-            <Route path="/" element={ <HomePage/> } />
+            <Route path="/" element={ <HomePage /> } />
             <Route exact path="/login" element={<Login />}/>
             <Route exact path="/register" element={<Register />}/>    
             <Route path="/category" element={<ProtectedRoutes auth={isLoggedIn}> <Category/> </ProtectedRoutes> } />
             <Route path="/product" element={<ProtectedRoutes auth={isLoggedIn}> <Product/> </ProtectedRoutes> } />
             <Route path="*" element={ <ErrorPage/> } />
-            <Route path='/category/:homeid' element={<HomeProdDetail/>}  ></Route>
-            <Route path='/cart' element={<Cartbtn/>}  ></Route>
-     
+            <Route path='/category/:homeid' element={<HomeProdDetail/>}  />
+            <Route path='/cart' element={<Cartbtn />}  />
+            <Route path='/payment' element={<Payment />}  />
+            
         </Routes>
     </div>
   )
