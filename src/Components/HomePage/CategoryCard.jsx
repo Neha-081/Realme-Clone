6ee5e221559804de31abc4892ac9c101710abb96
Neha-081 <nehaprajapati1150@gmail.com>
2022-03-16 18:02:22 +0530

    customized css

--- src/Components/HomePage/CategoryCard.jsx
+++ src/Components/HomePage/CategoryCard.jsx
@@ -3,16 +3,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 // import "./category.css";
- import "../../styles/category.css";
+import "../../styles/category.css";
 
 
 import { Pagination, Navigation } from "swiper";
 
-export default function CategoryCard({data}) {
+export default function CategoryCard({ data }) {
 
 
 
@@ -21,7 +21,7 @@ export default function CategoryCard({data}) {
     <>
 
 
-    <h2 className="title">Gone in Flash</h2>
+      <h2 className="title">Gone in Flash</h2>
       <Swiper
         slidesPerView={4}
         spaceBetween={30}
@@ -35,26 +35,26 @@ export default function CategoryCard({data}) {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-          
-          {data.filter((item)=>item.category==="tv").map((e)=>(
-              <div key={e.id} >
-                     
-    <SwiperSlide>
-    <Link to={`/category/${e.id}`} className="LinkTab" style={{textDecoration:"none"}}> 
-        <div>
-        <img src={e.image_url} alt="productImage" />
-           <h4 className="name">{e.name}</h4>
-        <h5 className="desc">{e.desc}</h5>
-        <h4 className="price">{e.price} <span className="cutprice">{e.cutPrice}</span></h4>
 
-        </div>
-        </Link>
-    </SwiperSlide>
-  
-    </div>
-    
-    
-))}
+        {data.filter((item) => item.category === "tv").map((e) => (
+          <div key={e.id} >
+
+            <SwiperSlide className="mainn">
+              <Link to={`/category/${e.id}`} className="LinkTab" style={{ textDecoration: "none" }}>
+                <div className="slider-div">
+                  <img src={e.image_url} alt="productImage" />
+                  <h4 className="name">{e.name}</h4>
+                  <h5 className="desc">{e.desc}</h5>
+                  <h4 className="price">{e.price} <span className="cutprice">{e.cutPrice}</span></h4>
+
+                </div>
+              </Link>
+            </SwiperSlide>
+
+          </div>
+
+
+        ))}
 
 
 
@@ -78,23 +78,23 @@ export default function CategoryCard({data}) {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-          {data.filter((item)=>item.category==="onlyhere").map((e)=>(
-              <div key={e.id}>
-                     
-    <SwiperSlide>
-    <Link to={`/category/${e.id}`} className="LinkTab" style={{textDecoration:"none"}}> 
-        <div>
-        <img src={e.image_url} alt="productImage" />
-        <h4 className="name">{e.name}</h4>
-        <h5 className="desc">{e.desc}</h5>
-        <h4 className="price">{e.price}</h4>
-        </div>
-        </Link>
-    </SwiperSlide>
+        {data.filter((item) => item.category === "onlyhere").map((e) => (
+          <div key={e.id}>
 
-    </div>
-    
-))}
+            <SwiperSlide  className="mainn">
+              <Link to={`/category/${e.id}`} className="LinkTab" style={{ textDecoration: "none" }}>
+                <div  className="slider-div">
+                  <img src={e.image_url} alt="productImage" />
+                  <h4 className="name">{e.name}</h4>
+                  <h5 className="desc">{e.desc}</h5>
+                  <h4 className="price">{e.price}</h4>
+                </div>
+              </Link>
+            </SwiperSlide>
+
+          </div>
+
+        ))}
 
 
 
